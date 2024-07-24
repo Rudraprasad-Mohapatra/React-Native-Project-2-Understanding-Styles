00aa7973ec9e12863203364c603b29163a42cec4
Rudraprasad-Mohapatra <rudraprasadmohapatra51@gmail.com>
2024-07-25 01:03:16 +0530

    Started with A Beginner's Guide to Images in React Native and Thanks to Hitesh Choudhary Sir

--- App.tsx
+++ App.tsx
@@ -2,6 +2,7 @@ import {View, Text, SafeAreaView, ScrollView} from 'react-native';
 import FlatCards from './components/FlatCards';
 import React from 'react';
 import ElevatedCards from './components/ElevatedCards';
+import FancyCard from './components/FancyCard';
 
 const App = () => {
   return (
@@ -9,6 +10,7 @@ const App = () => {
       <ScrollView>
         <FlatCards />
         <ElevatedCards />
+        <FancyCard />
       </ScrollView>
     </SafeAreaView>
   );
---
+++ components/FancyCard.tsx
@@ -0,0 +1,45 @@
+import {View, Text, StyleSheet, Image} from 'react-native';
+import React from 'react';
+
+export default function FancyCard() {
+  return (
+    <View>
+      <Text style={styles.headingText}>Trending Places</Text>
+      <View style={[styles.card, styles.cardElevated]}>
+        <Image
+          source={{
+            uri: 'https://images.pexels.com/photos/45853/grey-crowned-crane-bird-crane-animal-45853.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+          }}
+          style={styles.cardImage}
+        />
+        <View style={styles.cardBody}>
+          <Text style={styles.cardTitle}>Hawa Mahal</Text>
+          <Text style={styles.cardLabel}>Pink City, Jaipur</Text>
+          <Text style={styles.cardDescription}>
+            the City Palace.The Hawa Mahal is a palace in the city of Jaipur,
+            India. Built from Red and pink sandstone, it is on the edge of the Ciy Palace.
+          </Text>
+          <Text style={styles.cardFooter}>12 mins away</Text>
+        </View>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  headingText: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    paddingHorizontal: 8,
+  },
+  card: {},
+  cardElevated: {},
+  cardImage: {
+    height: 180,
+  },
+  cardBody: {},
+  cardTitle: {},
+  cardLabel: {},
+  cardDescription: {},
+  cardFooter: {},
+});
