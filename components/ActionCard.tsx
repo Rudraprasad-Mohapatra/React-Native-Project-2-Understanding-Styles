6c832cb47428c19b890bcac37814319253f9c3e1
Rudraprasad-Mohapatra <rudraprasadmohapatra51@gmail.com>
2024-08-01 00:41:12 +0530

    Accomplished the lecture 18 Iteration UI in React Native and Thanks yo Hitesh Choudhary Sir.

--- components/ActionCard.tsx
+++ components/ActionCard.tsx
@@ -9,7 +9,7 @@ import {
 import React from 'react';
 
 const ActionCard = () => {
-  function openWebsite(websiteLink: String) {
+  function openWebsite(websiteLink: string) {
     Linking.openURL(websiteLink);
   }
   return (
